faf21930b359daa7a7162fbc92ad5c5c8b3a302a
princemahmood117 <princemahmood117@gmail.com>
2024-09-25 02:35:26 +0600

    firebase hidden

--- craftNcrafts/src/firebase/firebase.config.js
+++ craftNcrafts/src/firebase/firebase.config.js
@@ -6,13 +6,15 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDAEZhpbw2EkRNeX8esjHo-YGlWkIHcneE",
-  authDomain: "craftncrafts-62ae9.firebaseapp.com",
-  projectId: "craftncrafts-62ae9",
-  storageBucket: "craftncrafts-62ae9.appspot.com",
-  messagingSenderId: "983909249293",
-  appId: "1:983909249293:web:2dadc2b5c4956034768044"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
+
+console.log(firebaseConfig);
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
